754e87ea5eecad023b811206cb5138991895260d
TJFord <tan.jifu@gmail.com>
2021-03-02 13:15:12 -0600

    New organization for the coupling code

---
+++ examples/embolism/build/CMakeFiles/embolism.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jifutan/bloodflow/examples/embolism/embolism.cpp" "/home/jifutan/bloodflow/examples/embolism/build/CMakeFiles/embolism.dir/embolism.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "PLB_MPI_PARALLEL"
+  "PLB_USE_POSIX"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/jifutan/bloodflow/examples/embolism/build/CMakeFiles/palabos.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/shared/openmpi-4.0.3/gcc-4.8.5/include"
+  "/home/jifutan/palabos/src"
+  "/home/jifutan/palabos/externalLibraries"
+  "/home/jifutan/palabos/externalLibraries/Eigen3"
+  "/home/jifutan/bloodflow/ibm"
+  "/home/jifutan/lammpsFeb2021/src"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ examples/embolism/build/CMakeFiles/embolism.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/embolism.dir/embolism.cpp.o"
+  "../embolism.pdb"
+  "../embolism"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/embolism.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
