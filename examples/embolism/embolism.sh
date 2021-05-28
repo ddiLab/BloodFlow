c88550fde8d32322a23b2c1a946a2cedfffc804b
Connor <murphyc@cooleylogin1.fst.alcf.anl.gov>
2021-05-28 16:21:33 +0000

    Final Draft

--- examples/embolism/embolism.sh
+++ examples/embolism/embolism.sh
@@ -2,6 +2,5 @@
 NODES=`cat $COBALT_NODEFILE | wc -l`
 PROCS=$((NODES * 12))
 #CONNOR: modify this file path
-set(EMBOLISM_ROOT "/home/murphyc/BloodFlow/examples/embolism")
-cd ${EMBOLISM_ROOT}  
+cd /home/murphyc/BloodFlow/examples/embolism 
 mpirun -f $COBALT_NODEFILE -n $PROCS ./embolism in.embolism
