e2cc900a940099d47192f83af4f9ba5e468a81dd
Connor <murphyc@cooleylogin2.fst.alcf.anl.gov>
2021-06-03 15:18:48 +0000

    testing cooley.cmake

--- examples/embolism/embolism.sh
+++ examples/embolism/embolism.sh
@@ -2,5 +2,5 @@
 NODES=`cat $COBALT_NODEFILE | wc -l`
 PROCS=$((NODES * 12))
 #CONNOR: modify this file path
-cd /home/murphyc/BloodFlow/examples/embolism 
-mpirun -f $COBALT_NODEFILE -n $PROCS ./embolism in.embolism
+EMB_PATH=/home/murphyc/BloodFlow/examples/embolism
+mpirun -f $COBALT_NODEFILE -n $PROCS $EMB_PATH/embolism $EMB_PATH/in.embolism
---
+++ sites/cooley.cmake
@@ -0,0 +1,4 @@
+set(PALABOS_ROOT "/home/murphyc/palabos")
+set(BLOODFLOW_ROOT "/home/murphyc/BloodFlow")
+set(LAMMPS_DIR "/home/murphyc/lammps")
+-DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_STANDARD=11
