1687715ccd60defeaee181f61f270ad8a8832617
Connor <murphyc@cooleylogin2.fst.alcf.anl.gov>
2021-06-04 14:39:20 +0000

    changes to cooley.cmake

--- sites/cooley.cmake
+++ sites/cooley.cmake
@@ -1,4 +1,5 @@
 set(PALABOS_ROOT "/home/murphyc/palabos")
 set(BLOODFLOW_ROOT "/home/murphyc/BloodFlow")
 set(LAMMPS_DIR "/home/murphyc/lammps")
--DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_STANDARD=11
+set(CMAKE_C_COMPILER "mpicc" CACHE STRING "Specify the compiler to use")
+set(CMAKE_CXX_STANDARD "11" CACHE STRING "Using c++11 version")
