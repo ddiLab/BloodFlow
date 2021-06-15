ebdb65e99d2f12800d47f01a12cff27212723324
Mectr0 <co.linus8@gmail.com>
2021-06-15 15:34:28 -0500

    Python code for writing a vtk

--- lmp2vtk/lmp2vtk.bash
+++ lmp2vtk/lmp2vtk.bash
@@ -1,3 +1,2 @@
- g++ -o lmp2vtk4s lmp2vtk4s.cpp #Creates an executable named lmp2vtk4s
+ g++ -o lmp2vtk4s lmp2vtk4s.cpp 
  ./lmp2vtk4s -p dump.rbc.xyz -t in.cells -w vtk/cell -s cell -c 3 -z 1
-#Information on parameters can befound in lmp2vtk4s.cpp
