6be6cd259a24e42479ded208f332ac9d2864c4c7
Mectr0 <co.linus8@gmail.com>
2021-10-11 15:26:45 -0500

    SenseiPersonnal.md

--- examples/Shear/Shear.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-NODES=`cat $COBALT_NODEFILE | wc -l`
-PROCS=$((NODES * 12))
-#CONNOR: modify this file path
-EMB_PATH=/home/mectro/BloodFlow/examples/Shear
-mpirun -f $COBALT_NODEFILE -n $PROCS $EMB_PATH/shear $EMB_PATH/in.lmp4cell
