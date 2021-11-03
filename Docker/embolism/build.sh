b6bfb9a3e978b328859e936d76aab1fcd00f6f9c
srizzi <srizzi@anl.gov>
2021-11-02 19:50:14 -0500

    singleCell building

--- Docker/embolism/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export IMAGENAME="srizzi/embolism:latest"
-
-docker build -t $IMAGENAME .
---
+++ Docker/singleCell/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# individuals user must edit this
+# environment variable to match their
+# system or docker hub username
+export IMAGENAME="srizzi/singlecell:latest"
+
+docker build -t $IMAGENAME .
